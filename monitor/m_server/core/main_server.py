60f52d77cf84b9ca0fa3192794852dc05f7de227
edward0316 <chenjiexuan123@hotmail.com>
2015-12-16 16:55:19 +1000

    commit

--- monitor/m_server/core/main_server.py
+++ monitor/m_server/core/main_server.py
@@ -1,6 +1,17 @@
 import global_setting
 from conf import hosts
 import redis_connector as redis
+import json
 
-for h in hosts.monitored_hosts:
-    print(h.hostname,h.ip_address)
+def push_configure_data_to_redis():
+
+    for h in hosts.monitored_hosts:
+        config_dic = {}
+
+        for k,v in h.services.items():
+            config_dic[k] = [v.interval, v.plugin_name]
+        print config_dic
+
+        redis.r['configuration::%s' % h.hostname] = json.dumps(config_dic)
+
+push_configure_data_to_redis()
