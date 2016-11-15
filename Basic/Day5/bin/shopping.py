61d5320d95d0eb1426b2412a3c5fb984b64567cf
edward0316 <chenjiexuan123@hotmail.com>
2016-11-15 22:32:39 +0800

    update

--- Basic/Day5/bin/shopping.py
+++ Basic/Day5/bin/shopping.py
@@ -1,3 +1,11 @@
+import GeneralSetting
 import sys
-sys.path.append("..")
-import conf.menu
+try:
+    cmd = sys.argv[1]
+    if cmd == "start":
+        import conf.menu
+    else:
+        print("Please add 'start' as the parameter")
+except:
+    print("Please add 'start' as the parameter")
+
